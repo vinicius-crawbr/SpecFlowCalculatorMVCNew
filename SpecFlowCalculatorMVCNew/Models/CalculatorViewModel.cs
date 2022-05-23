896fbcf38d107f074e5eda87d46bd8fd263996c9
Vinicius Oliveira <vinicius.oliveira@crawfordbrasil.com.br>
2022-05-23 08:37:05 -0300

    Initial commit

---
+++ SpecFlowCalculatorMVCNew/Models/CalculatorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpecFlowCalculatorMVCNew.Models
+{
+    public class CalculatorViewModel
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Result { get; set; }
+    }
+}
